f09f998a56b0831e01c267d3d41257d54a7f7aa9
Vladas Maier <vladas.maier@goyahealth.de>
2023-04-15 19:12:06 +0200

    Refactor application

--- app/src/main/java/com/vladas/baseswitch/MainActivity.java
+++ app/src/main/java/com/vladas/baseswitch/MainActivity.java
@@ -1,11 +1,7 @@
 package com.vladas.baseswitch;
 
 import android.app.Activity;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,91 +15,83 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
 
-        Typeface font = Typeface.createFromAsset(getAssets(), "ostrich-regular.ttf");
+        EditText decimalInput = findViewById(R.id.number_decimal);
+        EditText binaryInput = findViewById(R.id.number_binary);
+        EditText octalInput = findViewById(R.id.number_octal);
+        EditText hexadecimalInput = findViewById(R.id.number_hexadecimal);
 
-        TextView header = (TextView) findViewById(R.id.header);
-        header.setTypeface(font);
-        TextView header10 = (TextView) findViewById(R.id.header10);
-        header10.setTypeface(font);
-        TextView header2 = (TextView) findViewById(R.id.header2);
-        header2.setTypeface(font);
-        TextView header8 = (TextView) findViewById(R.id.header8);
-        header8.setTypeface(font);
-        TextView header16 = (TextView) findViewById(R.id.header16);
-        header16.setTypeface(font);
+        setDefaultIfEmpty(decimalInput);
+        setDefaultIfEmpty(binaryInput);
+        setDefaultIfEmpty(octalInput);
+        setDefaultIfEmpty(hexadecimalInput);
 
-        EditText z10 = (EditText) findViewById(R.id.z10);
-        z10.setTypeface(font);
-        EditText z2 = (EditText) findViewById(R.id.z2);
-        z2.setTypeface(font);
-        EditText z8 = (EditText) findViewById(R.id.z8);
-        z8.setTypeface(font);
-        EditText z16 = (EditText) findViewById(R.id.z16);
-        z16.setTypeface(font);
+        TextView header = findViewById(R.id.header);
 
-        Button b = (Button) findViewById(R.id.b);
-        b.setTypeface(font);
-        Button c = (Button) findViewById(R.id.c);
-        c.setTypeface(font);
+        Button convertButton = findViewById(R.id.button_convert);
 
-        b.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                TextView header = (TextView) findViewById(R.id.header);
-                EditText z10 = (EditText) findViewById(R.id.z10);
-                EditText z2 = (EditText) findViewById(R.id.z2);
-                EditText z8 = (EditText) findViewById(R.id.z8);
-                EditText z16 = (EditText) findViewById(R.id.z16);
-
-                if (z10.getText().length() != 0 && z2.getText().length() == 0 && z8.getText().length() == 0 && z16.getText().length() == 0) {
-                    header.setText("Geben Sie die Zahl entsprechend ein");
-                    int input = Integer.parseInt(z10.getText().toString());
-
-                    z2.setText(Integer.toString(input, 2));
-                    z8.setText(Integer.toString(input, 8));
-                    z16.setText(Integer.toString(input, 16));
-                } else if(z2.getText().length() != 0 && z8.getText().length() == 0 && z10.getText().length() == 0 && z16.getText().length() == 0) {
-                    header.setText("Geben Sie die Zahl entsprechend ein");
-                    String input = z2.getText().toString();
-                    int dez = Integer.parseInt(input, 2);
-
-                    z10.setText(Integer.toString(dez, 10));
-                    z8.setText(Integer.toString(dez, 8));
-                    z16.setText(Integer.toString(dez, 16));
-                } else if(z8.getText().length() != 0 && z2.getText().length() == 0 && z10.getText().length() == 0 && z16.getText().length() == 0) {
-                    header.setText("Geben Sie die Zahl entsprechend ein");
-                    String input = z8.getText().toString();
-                    int dez = Integer.parseInt(input, 8);
-
-                    z10.setText(Integer.toString(dez, 10));
-                    z2.setText(Integer.toString(dez, 2));
-                    z16.setText(Integer.toString(dez, 16));
-                } else if(z16.getText().length() != 0 && z2.getText().length() == 0 && z8.getText().length() == 0 && z10.getText().length() == 0) {
-                    header.setText("Geben Sie die Zahl entsprechend ein");
-                    String input = z16.getText().toString();
-                    int dez = Integer.parseInt(input, 16);
-
-                    z10.setText(Integer.toString(dez, 10));
-                    z2.setText(Integer.toString(dez, 2));
-                    z8.setText(Integer.toString(dez, 8));
-                } else
-                    header.setText("Falsche Eingabe!");
+        convertButton.setOnClickListener(v -> {
+            long number = 0L;
+            if (decimalInput.hasFocus()) {
+                number = Integer.parseInt(decimalInput.getText().toString(), 10);
+            } else if (binaryInput.hasFocus()) {
+                number = Integer.parseInt(binaryInput.getText().toString(), 2);
+            } else if (octalInput.hasFocus()) {
+                number = Integer.parseInt(octalInput.getText().toString(), 8);
+            } else if (hexadecimalInput.hasFocus()) {
+                number = Integer.parseInt(hexadecimalInput.getText().toString(), 16);
             }
+            setValues(number);
         });
 
-        c.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                EditText z10 = (EditText) findViewById(R.id.z10);
-                EditText z2 = (EditText) findViewById(R.id.z2);
-                EditText z8 = (EditText) findViewById(R.id.z8);
-                EditText z16 = (EditText) findViewById(R.id.z16);
+        Button clearButton = findViewById(R.id.button_clear);
 
-                z2.setText("");
-                z8.setText("");
-                z10.setText("");
-                z16.setText("");
-            }
+        clearButton.setOnClickListener(v -> {
+            header.setText(getString(R.string.ask_for_number));
+            binaryInput.setText(DEFAULT);
+            octalInput.setText(DEFAULT);
+            decimalInput.setText(DEFAULT);
+            hexadecimalInput.setText(DEFAULT);
         });
+
     }
+
+    private void setDefaultIfEmpty(EditText editText) {
+        if (editText.getText().toString().isEmpty()) {
+            editText.setText(DEFAULT);
+        }
+    }
+
+    private void setValues(long number) {
+        setBinaryValue(number);
+        setOctalValue(number);
+        setDecimalValue(number);
+        setHexadecimalValue(number);
+    }
+
+    private void setBinaryValue(long number) {
+        EditText binaryInput = findViewById(R.id.number_binary);
+        binaryInput.setText(String.format(Long.toString(number, 2)));
+    }
+
+    private void setOctalValue(long number) {
+        EditText octalInput = findViewById(R.id.number_octal);
+        octalInput.setText(String.format(Long.toString(number, 8)));
+    }
+
+    private void setDecimalValue(long number) {
+        EditText decimalInput = findViewById(R.id.number_decimal);
+        decimalInput.setText(String.format(Long.toString(number, 10)));
+    }
+
+    private void setHexadecimalValue(long number) {
+        EditText hexadecimalInput = findViewById(R.id.number_hexadecimal);
+        hexadecimalInput.setText(String.format(Long.toString(number, 16)).toUpperCase());
+    }
+
+    private static final String DEFAULT = "0";
 }
